3f45c3848295d8eba88a588d37434a3d78cb4527
DILEESHA\dlaks <dlakshansamaraweera@gmail.com>
2024-06-07 11:21:18 +0530

    Add signUp.dart file for create signup screen and add header,inputFeild,signUp button.

---
+++ dileesha/frontend/lib/signUp.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class signUp extends StatefulWidget {
+  const signUp({super.key});
+
+  @override
+  State<signUp> createState() => _signUpState();
+}
+
+class _signUpState extends State<signUp> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(child: Scaffold(
+      backgroundColor: Color.fromRGBO(229, 202, 119, 30),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(30),
+        child: Column(
+            children: [
+              _header(context),
+              _inputFeild(context),
+              _elevateButton(context),
+            ]
+        ),
+      ),
+    ),
+    );
+  }
+
+  _header(context) {
+    return Column(
+      children: <Widget>[
+        Text("Register",
+            style: TextStyle(fontSize: 40, fontWeight: FontWeight.w900),
+            textAlign: TextAlign.left),
+        Text("Please Register Into Continue",
+          textAlign: TextAlign.left,
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+      ],
+    );
+  }
+
+  _inputFeild(context){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        TextField(
+          decoration: InputDecoration(
+              hintText: "User Name",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(25),
+              ),
+              fillColor: Theme
+                  .of(context)
+                  .primaryColorLight
+                  .withOpacity(0.2),
+              filled: true,
+              prefixIcon: Icon(Icons.person)
+          ),
+        ),
+        SizedBox(height: 10),
+
+        TextField(
+          decoration: InputDecoration(
+              hintText: "Phone Number",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(25),
+              ),
+              fillColor: Theme
+                  .of(context)
+                  .primaryColorLight
+                  .withOpacity(0.2),
+              filled: true,
+              prefixIcon: Icon(Icons.phone)
+          ),
+        ),
+        SizedBox(height: 10,),
+
+        TextField(
+          decoration: InputDecoration(
+            hintText: "Password",
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(25),
+            ),
+            fillColor: Theme
+                .of(context)
+                .primaryColorLight
+                .withOpacity(0.2),
+            filled: true,
+            prefixIcon: Icon(Icons.lock_rounded),
+            suffixIcon: Icon(Icons.remove_red_eye_outlined),
+          ),
+          obscureText: true,
+        ),
+        SizedBox(height: 30,)
+      ],
+    );
+  }
+
+  _elevateButton(context){
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        ElevatedButton(onPressed: (){}, child:Text("Sign Up",style: TextStyle(fontSize: 20),),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.amber),
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.black),
+            shape:MaterialStateProperty.all(StadiumBorder()),
+            padding: MaterialStateProperty.all(EdgeInsets.symmetric(vertical: 10)) ,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
